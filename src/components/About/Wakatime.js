c62f78208c8bc9940bbcf328090f5f1c034723df
zdienos <zdienos@yahoo.com>
2022-08-04 00:07:29 +0800

    fix arrow function chart

--- src/components/About/Wakatime.js
+++ src/components/About/Wakatime.js
@@ -5,8 +5,9 @@ import * as am4charts from "@amcharts/amcharts4/charts";
 import am4themes_animated from "@amcharts/amcharts4/themes/animated";
 import fetchJsonp from "fetch-jsonp";
 
-// const WakaChartURL = 'https://wakatime.com/share/@zdienos/87859afd-b87c-4e94-899e-a42ffe0f7b72.json'; //last year
-const WakaChartURL = "https://wakatime.com/share/@zdienos/0f82529f-2922-478d-afab-5b78745f01a8.json"; //30 day
+const WakaChartURL =
+  "https://wakatime.com/share/@zdienos/87859afd-b87c-4e94-899e-a42ffe0f7b72.json"; //last year
+// const WakaChartURL = "https://wakatime.com/share/@zdienos/0f82529f-2922-478d-afab-5b78745f01a8.json"; //30 day
 // const WakaChartURL = 'https://wakatime.com/share/@zdienos/ba313035-ceeb-48b6-91fc-784abe4c4329.json'; //all the time
 // const WakaChartURL =  "https://wakatime.com/share/@zdienos/618218b9-cc3f-4100-bcf5-5f1e6c0a76d6.json"; //last 7 days
 
@@ -15,9 +16,8 @@ am4core.useTheme(am4themes_animated);
 function Wakatime() {
   const chart = useRef(null);
 
-  function createChart(chartData) {
-    console.log('data',chartData);
-
+  // function createChart(chartData) {
+  const createChart = (chartData) => {
     let x = am4core.create("chartdiv", am4charts.XYChart);
 
     x.paddingRight = 20;
@@ -32,7 +32,7 @@ function Wakatime() {
     categoryAxis.renderer.minGridDistance = 1;
     categoryAxis.renderer.inversed = true;
     categoryAxis.renderer.grid.template.disabled = true;
-    categoryAxis.title.text = "Languages over last 30 days";
+    categoryAxis.title.text = "Languages over last year";
 
     let valueAxis = x.xAxes.push(new am4charts.ValueAxis());
     valueAxis.min = 0;
@@ -46,11 +46,11 @@ function Wakatime() {
     series.columns.template.column.cornerRadiusTopRight = 5;
 
     series.columns.template.adapter.add("fill", function (fill, target) {
-        return target.dataItem.dataContext.color;
+      return target.dataItem.dataContext.color;
     });
     series.columns.template.adapter.add("stroke", function (fill, target) {
-        return target.dataItem.dataContext.color;
-    });    
+      return target.dataItem.dataContext.color;
+    });
 
     // untuk label di setiap seriesnya
     // let labelBullet = series.bullets.push(new am4charts.LabelBullet());
@@ -59,7 +59,7 @@ function Wakatime() {
     // labelBullet.label.text =
     //   "{values.valueX.workingValue.formatNumber('#.0as')}";
     // labelBullet.locationX = 1;
-    
+
     // defaulnya untuk column dengan seriers yang sama, warnanya sama, tambahkan adapter untuk mengambil warna dari chart.colors set
     // series.columns.template.adapter.add("fill", function (fill, target) {
     //   return x.colors.getIndex(target.dataItem.index);
@@ -82,7 +82,7 @@ function Wakatime() {
           return response.json();
         })
         .then(function (response) {
-          createChart(response.data);          
+          createChart(response.data);
         })
         .catch(function (ex) {
           console.log("parsing failed", ex);
