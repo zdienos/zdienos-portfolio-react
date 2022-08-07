36578c26bce305cafa9f9908b951af428167d408
zdienos <zdienos@yahoo.com>
2022-08-07 11:49:26 +0800

    tambah lazy loading suspense

--- src/Main.js
+++ src/Main.js
@@ -1,17 +1,29 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, lazy, Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+
 import Preloader from "../src/components/Pre";
-import Navbar from "./components/Navbar";
-import Home from "./components/Home/Home";
-import About from "./components/About/About";
-import Projects from "./components/Projects/Projects";
-import Footer from "./components/Footer";
-import Resume from "./components/Resume/Resume";
-import ScrollToTop from "./components/ScrollToTop";
 
 import "./main.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+
+
+//import Navbar from "./components/Navbar";
+const Navbar  = lazy(() => import("./components/Navbar"));
+// import Home from "./components/Home/Home";
+const Home  = lazy(() => import("./components/Home/Home"));
+// import About from "./components/About/About";
+const About  = lazy(() => import("./components/About/About"));
+// import Projects from "./components/Projects/Projects";
+const Projects  = lazy(() => import("./components/Projects/Projects"));
+// import Footer from "./components/Footer";
+const Footer  = lazy(() => import("./components/Footer"));
+// import Resume from "./components/Resume/Resume";
+const Resume  = lazy(() => import("./components/Resume/Resume"));
+// import ScrollToTop from "./components/ScrollToTop";
+const ScrollToTop  = lazy(() => import("./components/ScrollToTop"));
+
+
 
 
 function Main() {  
@@ -27,17 +39,22 @@ function Main() {
 
   return (
     <Router>
-      <Preloader load={load} />
-      <div className="App" id={load ? "no-scroll" : "scroll"}>      
-        <Navbar />
-        <ScrollToTop />
+      {/* <Preloader load={load} /> */}
+      <div className="App" id={load ? "no-scroll" : "scroll"}>  
+      {/* <Suspense fallback={<h1>Still Loading…</h1>}> */}
+      <Suspense fallback={<Preloader />}> <Navbar /> </Suspense>
+      <Suspense fallback={<Preloader />}>   <ScrollToTop /> </Suspense>
+      <Suspense fallback={<Preloader />}>  
         <Routes>
-          <Route path="/" exact element={<Home />} />
-          <Route path="/project" element={<Projects />} />
-          <Route path="/about" element={<About/>} />
-          <Route path="/resume" element={<Resume/>} />
+          <Route path="/" exact element={<Home />} /> 
+          <Route path="/project" element={<Projects />} /> 
+          <Route path="/about" element={<About/>} /> 
+          <Route path="/resume" element={<Resume/>} /> 
         </Routes>
-        <Footer />
+        </Suspense>
+        <Suspense fallback={<Preloader />}> <Footer /> </Suspense>
+      
+        
       </div>
     </Router>
   );
--- src/components/Pre.js
+++ src/components/Pre.js
@@ -1,6 +1,9 @@
 import React from "react";
-function Pre(props) {
-  return <div id={props.load ? "preloader" : "preloader-none"}></div>;
+// function Pre(props) {
+//   return <div id={props.load ? "preloader" : "preloader-none"}></div>;
+// }
+function Pre() {
+  return <div id={"preloader"}></div>;
 }
 
 export default Pre;
