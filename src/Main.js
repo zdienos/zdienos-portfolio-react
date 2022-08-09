713dc31f32ca3a59cfa6e85a200d83e1d39415f2
zdienos <zdienos@yahoo.com>
2022-08-09 20:40:16 +0800

    done darkmode

--- src/Main.js
+++ src/Main.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect, lazy, Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import useLocalStorage from 'use-local-storage'
 
 
 import Preloader from "../src/components/Pre";
 
 import "./main.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BsFillMoonFill, BsFillSunFill } from "react-icons/bs";
+import DarkModeToggle from "./components/DarkModeToggle";
+// import { BsFillMoonFill, BsFillSunFill } from "react-icons/bs";
 
 
 
@@ -31,15 +31,8 @@ const ScrollToTop  = lazy(() => import("./components/ScrollToTop"));
 function Main() {  
   const [load, upadateLoad] = useState(true);  
   
-  const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-  const [theme, setTheme] = useLocalStorage('theme', defaultDark ? 'dark' : 'light');
-
-  const switchTheme = () => {
-    const newTheme = theme === 'light' ? 'dark':'light';    
-    setTheme(newTheme);
-  }
   
-  useEffect(() => {
+  useEffect(() => {    
     const timer = setTimeout(() => {
       upadateLoad(false);
     }, 1200);
@@ -50,22 +43,9 @@ function Main() {
   return (
     <Router>      
       {/* <Preloader load={load} /> */}
-      <div className="App" id={load ? "no-scroll" : "scroll"} data-theme={theme}> 
-      {/* <Suspense fallback={<h1>Still Loading…</h1>}> */}
-      
-        <label className="float" >
-          <input className='toggle-checkbox' type='checkbox' onClick={switchTheme}></input>
-          <div className='toggle-slot'>
-            <div className='moon-icon-wrapper'>
-              <BsFillSunFill className="sun-icon"/>
-            </div>
-            <div className='toggle-button'></div>
-            <div className='sun-icon-wrapper'>
-              <div className="bi bi-moon"></div>
-              <BsFillMoonFill className="moon-icon"/>
-            </div>
-          </div>
-        </label>
+      <div className="App" id={load ? "no-scroll" : "scroll"}> 
+                      
+      <DarkModeToggle/>
 
       <Suspense fallback={<Preloader />}> <Navbar /> </Suspense>
       <Suspense fallback={<Preloader />}>   <ScrollToTop /> </Suspense>
@@ -78,8 +58,7 @@ function Main() {
         </Routes>
         </Suspense>
         <Suspense fallback={<Preloader />}> <Footer /> </Suspense>
-      
-        
+                      
       </div>
     </Router>
   );
---
+++ src/components/DarkModeToggle.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react";
+import { BsFillMoonFill, BsFillSunFill } from "react-icons/bs";
+
+const DarkModeToggle = () => {
+  const [checked, setChecked] = useState(
+    localStorage.getItem("theme") === "dark" ? true : false
+  );
+  useEffect(() => {
+    document
+      .getElementsByTagName("HTML")[0]
+      .setAttribute("data-theme", localStorage.getItem("theme"));
+  }, []);
+
+  const toggleThemeChange = () => {
+    if (checked === false) {
+      localStorage.setItem("theme", "dark");
+      document
+        .getElementsByTagName("HTML")[0]
+        .setAttribute("data-theme", localStorage.getItem("theme"));
+      setChecked(true);
+    } else {
+      localStorage.setItem("theme", "light");
+      document
+        .getElementsByTagName("HTML")[0]
+        .setAttribute("data-theme", localStorage.getItem("theme"));
+      setChecked(false);
+    }
+  };
+
+  return (
+    <label className="float">
+      <input
+        className="toggle-checkbox"
+        type="checkbox"
+        defaultChecked={checked}
+        onChange={() => toggleThemeChange()}
+      ></input>
+      <div className="toggle-slot">
+        <div className="moon-icon-wrapper">
+          <BsFillSunFill className="sun-icon" />
+        </div>
+        
+        <div className="sun-icon-wrapper">
+          <div className="bi bi-moon"></div>
+          <BsFillMoonFill className="moon-icon" />
+        </div>
+      </div>
+    </label>
+  );
+};
+
+export default DarkModeToggle;
